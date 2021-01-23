9a6fbe9f9715e65c760b65aa96b1ba970f9f4e5c
MamoruDS <mamoruds.io@gmail.com>
2021-01-23 22:46:53 +0900

    added: install_neovim.sh

---
+++ install_neovim.sh
@@ -0,0 +1,11 @@
+if ! [ -x "$(command -v curl)" ]; then
+  echo 'Error: curl is not installed' >&2
+  exit 1
+fi
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz \
+    && tar -zxf nvim-linux64.tar.gz \
+    && rm -rf nvim-linux64/share/man \
+    && cp -r nvim-linux64/* /usr/local/
+
+rm -rf nvim-linux64.tar.gz nvim-linux64 &> /dev/null
