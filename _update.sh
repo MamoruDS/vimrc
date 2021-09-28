a98636396d88ec399c2979892f7d8d28a144b94d
MamoruDS <mamoruds.io@gmail.com>
2021-09-28 09:30:50 +0900

    improved: intro sha checking in update scripts

--- _update.sh
+++ _update.sh
@@ -12,6 +12,20 @@ for i in "$@"; do
     esac
 done
 
+SHA_PATH=$HOME/.config/nvim/current_sha
+SHA=$(curl -s https://api.github.com/repos/MamoruDS/vimrc/commits/main | grep 'sha' | sed -r 's/^[^:]*:\ "(.*)",$/\1/' | head -1)
+EXIST_SHA="unknown"
+if [ -f $SHA_PATH ]; then
+    EXIST_SHA=$(cat $SHA_PATH)
+fi
+echo "fetching: ${EXIST_SHA:0:7} -> ${SHA:0:7}"
+if [ "$SHA" = "$EXIST_SHA" ]; then
+    if ! [ "$INIT" = true ]; then
+        echo "nothing to update, exiting..."
+        exit 0
+    fi
+fi
+
 if [ "$INIT" = true ]; then
     curl -sL https://raw.githubusercontent.com/MamoruDS/vimrc/main/init.vim > $_ROOT/init.vim 
 fi
@@ -43,3 +57,6 @@ conf="syn_typescript"
 curl -sL https://raw.githubusercontent.com/MamoruDS/vimrc/main/custom/$conf.vim > $_CUSTOM/$conf.vim
 conf="syn_vim"
 curl -sL https://raw.githubusercontent.com/MamoruDS/vimrc/main/custom/$conf.vim > $_CUSTOM/$conf.vim
+
+echo $SHA > $SHA_PATH
+echo 'done.'
--- update.sh
+++ update.sh
@@ -12,6 +12,20 @@ for i in "$@"; do
     esac
 done
 
+SHA_PATH=$HOME/.config/nvim/current_sha
+SHA=$(curl -s https://api.github.com/repos/MamoruDS/vimrc/commits/main | grep 'sha' | sed -r 's/^[^:]*:\ "(.*)",$/\1/' | head -1)
+EXIST_SHA="unknown"
+if [ -f $SHA_PATH ]; then
+    EXIST_SHA=$(cat $SHA_PATH)
+fi
+echo "fetching: ${EXIST_SHA:0:7} -> ${SHA:0:7}"
+if [ "$SHA" = "$EXIST_SHA" ]; then
+    if ! [ "$INIT" = true ]; then
+        echo "nothing to update, exiting..."
+        exit 0
+    fi
+fi
+
 if [ "$INIT" = true ]; then
     curl -sL https://raw.githubusercontent.com/MamoruDS/vimrc/main/init.vim > $_ROOT/init.vim 
 fi
@@ -38,3 +52,6 @@ fi
 for conf in "${confs[@]}"; do
     curl -sL https://raw.githubusercontent.com/MamoruDS/vimrc/main/custom/$conf.vim > $_CUSTOM/$conf.vim
 done
+
+echo $SHA > $SHA_PATH
+echo 'done.'
